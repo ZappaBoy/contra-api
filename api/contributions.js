0b70e4aab1a04b524e91abf49422a4ed2e6c80da
Federico Zappone <federico.zappone@justanother.cloud>
2025-09-10 00:54:25 +0200

    feat: add multiple usernames

--- api/contributions.js
+++ api/contributions.js
@@ -2,6 +2,15 @@ const router = require('express').Router()
 
 const { githubContributionData, gitlabContributionData } = require('../requests')
 
+function appendContribution(contributions, contribution) {
+    const index = contributions.findIndex(item => item.date === contribution.date)
+    if (index !== -1) {
+        contributions[index].count = contributions[index].count + contribution.count
+    } else {
+        contributions.push(contribution)
+    }
+}
+
 router.get('/', async (req, res) => {
     const githubUsername = req.query.githubUsername
     const gitlabUsername = req.query.gitlabUsername
@@ -17,30 +26,39 @@ router.get('/', async (req, res) => {
      */
     let contributions = []
 
-    await githubContributionData(githubUsername).then(data => {
-        data.forEach(element => {
-            element.contributionDays.forEach(day => {
-                contributions.push({
-                    date: day.date,
-                    count: day.contributionCount
+    const githubUsernames = githubUsername.split(',').map(name => name.trim())
+    const gitlabUsernames = gitlabUsername.split(',').map(name => name.trim())
+
+    for (const username of githubUsernames) {
+        await githubContributionData(username).then(data => {
+            data.forEach(element => {
+                element.contributionDays.forEach(day => {
+                    appendContribution(contributions, {
+                        date: day.date,
+                        count: day.contributionCount
+                    })
                 })
-            })
+            });
+        })
+    }
+
+    for (const username of gitlabUsernames) {
+
+        let gitlabData = await gitlabContributionData(username).then(data => {
+            return Object.entries(data).map(([key, value]) => ({date: key, count: value}))
+        })
+
+        contributions.forEach((element, index) => {
+            const itemIndex = gitlabData.findIndex(item => item.date === element.date)
+
+            if (itemIndex !== -1) {
+                appendContribution(contributions, {
+                    date: contributions[index].date,
+                    count: contributions[index].count
+                })
+            }
         });
-    })
-
-    let gitlabData = []
-
-    await gitlabContributionData(gitlabUsername).then(data => {
-        gitlabData = Object.entries(data).map(([key, value]) => ({ date: key, count: value }))
-    })
-
-    contributions.forEach((element, index) => {
-        const itemIndex = gitlabData.findIndex(item => item.date === element.date)
-
-        if (itemIndex !== -1) {
-            contributions[index].count = contributions[index].count + gitlabData[itemIndex].count
-        }
-    });
+    }
 
     const totalContributionCount = contributions.reduce((accumulator, contribution) => {
         return accumulator + contribution.count;
